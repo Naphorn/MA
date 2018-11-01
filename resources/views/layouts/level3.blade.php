0cc04a3b487d91807d96486d8d46f7587f491e69
Woraprad <31096955+Woraprad@users.noreply.github.com>
2018-11-02 01:38:11 +0700

    Update resources/views/layouts/level3.blade.php
    พิมพ์คำสั่งแสดงข้อมูลในตาราง

--- resources/views/layouts/level3.blade.php
+++ resources/views/layouts/level3.blade.php
@@ -7,11 +7,11 @@
   <div class="row">
       <div class="col-md-12">
       <br><br>
-      {{-- @if(\Session::has('success'))
+      @if(\Session::has('success'))
           <div class="alert alert-success">
               <p>{{\Session::get('success')}}</p>
           </div>
-      @endif --}}
+      @endif
       <div align="right">{{-- <a href="{{route('user.create')}}"class="btn btn-success">เพิ่มข้อมูลผู้ใช้</a>--}}</div>
           <table {{--class="table table-bordered table-striped"--}} align="center" border="1"  width="70%"max-width="70%"margin-bottom="22px">
               <tr>
@@ -24,21 +24,28 @@
                   <th>ประสิทธิภาพ</th>
                   <th>สถานะ</th>
               </tr>
-              {{-- @foreach($users as $row)
+               @foreach($users as $row)
                   <tr>
-                      <td>{{$row['id']}}</td>
-                      <td>{{$row['fname']}}</td>
-                      <td>{{$row['lname']}}</td>
-                      <td><a href="{{action('UsersController@edit',$row['id'])}}" class="btn btn-warning">Edit</a></td>
+                      <td>{{$row['IP']}}</td>
+                      <td>{{$row['Tempcool']}}</td>
+                      <td>{{$row['TempRoom']}}</td>
+                      <td>{{$row['voltCool']}}</td>
+                      <td>{{$row['CurrentCool']}}</td>
+                      <td>{{$row['CurrentHot']}}</td>
+                      <td>{{$row['WindSpeed']}}</td>
+                      <td>{{$row['Performance']}}</td>
+                      <td>{{$row['Power']}}</td>
+
+                      {{-- <td><a href="{{action('UsersController@edit',$row['id'])}}" class="btn btn-warning">Edit</a></td>
                       <td>
                           <form method="post" class="delete_form" action="{{action('UsersController@destroy',$row['id'])}}">
                               {{csrf_field()}}
                               <input type="hidden" name="_method" value="DELETE" />
                               <button type="submit" class="btn btn-danger">Delete</button>
                           </form>
-                      </td>
+                      </td> --}}
                   </tr>
-              @endforeach --}}
+              @endforeach
           </table>
       </div>
   </div>
