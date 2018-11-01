89c62cb7bd932deef81c0baabf8f41a57e5ce999
Naphorn Chokpokasombat <31033972+Naphorn@users.noreply.github.com>
2018-11-02 01:16:02 +0700

    add table status

---
+++ database/migrations/2018_11_01_172524_create_statusair_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusairTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statusair', function (Blueprint $table) {
+          $table->string('IP',100);
+          $table->float('TempCool');
+          $table->float('TempRoom');
+          $table->float('VoltCool');
+          $table->float('CurrentCool');
+          $table->float('CurrentHot');
+          $table->float('WindSpeed');
+          $table->float('Performance',50);
+          $table->char('Power',50);
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statusair');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,5 +24,29 @@
             </div>
         </div>
     </div>
+
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Status</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+        </tr>
+      </tbody>
+    </table>
+
 </div>
 @endsection
