194b40b99e34568bd81e5ae1cbe17e7633f0f360
Naphorn Chokpokasombat <31033972+Naphorn@users.noreply.github.com>
2018-11-02 13:46:36 +0700

    edit Paragraph home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,73 +21,75 @@
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title" id="exampleModalCenterTitle">Status</h1>
-          </div>
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title" id="exampleModalCenterTitle">Status</h1>
+                </div>
 
-          <div class="modal-body">
+                <div class="modal-body">
 
-            <table class="table table-dark">
-              <thead>
-                <tr>
-                  <th scope="col">Status</th>
-                  <th scope="col">-----</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">IP</th>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">อุณหภูมิคอยล์เย็น</th>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">อุณหภูมิห้อง</th>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">แรงดันคอยล์เย็น</th>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">กระแสคอยล์ร้อน</th>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">ความเร็วลม</th>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">ประสิทธิภาพ</th>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">สถานะ</th>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
+                    <table class="table table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">Status</th>
+                                <th scope="col">-----</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">IP</th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">อุณหภูมิคอยล์เย็น</th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">อุณหภูมิห้อง</th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">แรงดันคอยล์เย็น</th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">กระแสคอยล์ร้อน</th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">ความเร็วลม</th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">ประสิทธิภาพ</th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">สถานะ</th>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
 
-            <thead>
-              <tr>
-                <th scope="row">Switch</th>
-              </tr>
-              <tr>
-                <th scope="col"><button type="button" class="btn btn-success">ON</button></th>
-                <th scope="col"><button type="button" class="btn btn-danger">OFF</button></th>
-              </tr>
-            </thead>
+                    <thead>
+                    <tr>
+                    <th scope="row">Switch</th>
+                    </tr>
+                    <tr>
+                    <th scope="col"><button type="button" class="btn btn-success">ON</button></th>
+                    <th scope="col"><button type="button" class="btn btn-danger">OFF</button></th>
+                    </tr>
+                    </thead>
 
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+
+            </div>
         </div>
-      </div>
     </div>
 
 </div>
