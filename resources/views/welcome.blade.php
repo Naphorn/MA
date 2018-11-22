c2f3b04d5f1a7897f248b4943dcc1ac50c872fcd
Naphorn <naphorn.chok@gmail.com>
2018-11-22 22:23:19 +0700

    Format 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,11 @@
 @extends('layouts.app')
 @section('title','ManagementAir')
 @section('content')
-                <div class="flex-container">
-                    <div class="content">
-                        <p class="WelcomePage">
-                            ManagementAir
-                        </p>
-                
-                <!-- <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div> -->
-            </div>
+    <div class="flex-container">
+        <div class="content">
+            <p class="WelcomePage">
+                ManagementAir
+            </p>
         </div>
+    </div>
 @endsection
